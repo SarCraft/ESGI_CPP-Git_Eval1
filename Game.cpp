fad9e64419d1bee608151f2823144a5e5eebfcb1
raphael.bedleem <rbedleem@myges.fr>
2025-12-04 15:11:18 +0100

    feat: add Game Mode

--- Game.cpp
+++ Game.cpp
@@ -1,14 +1,28 @@
 #include "Game.h"
+#include "pvp.h"
+#include "pve.h"
 #include <iostream>
 
 using namespace std;
 
 void Game::start()
 {
-    // selectNames();
-    // grid.display();
-    // currentPlayer = &j1;
-    // turn();
+    Pvp pvp;
     Pve pve;
-    pve.start();
+
+
+    int modeJeu;
+    cout << "Choisissez votre mode de jeu : 1 = 1_Joueur, 2 = 2_Joueurs" <<endl;
+    cin >> modeJeu;
+    switch (modeJeu) {
+        case 1:
+            pve.start();
+            break;
+        case 2:
+            pvp.start();
+            break;
+        default:
+            cout << "Entree invalide" << endl;
+            break;
+    }
 }
